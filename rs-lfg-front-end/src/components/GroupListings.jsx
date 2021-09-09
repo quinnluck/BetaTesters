f4d06e97413c883e4453a05847e7b93037d9dd0f
Quinn Luck <quinnluck17@gmail.com>
2021-09-09 16:01:09 -0600

    got a working basic table in group listings

--- rs-lfg-front-end/src/components/GroupListings.jsx
+++ rs-lfg-front-end/src/components/GroupListings.jsx
@@ -1,6 +1,16 @@
 import React from 'react'
 import { withStyles } from '@material-ui/core/styles';
-import { Container } from "@material-ui/core";
+import {
+    Container,
+    Table,
+    TableBody,
+    TableCell,
+    TableContainer,
+    TableHead,
+    TableRow,
+    Paper,
+    unstable_createMuiStrictModeTheme
+} from "@material-ui/core";
 import Label from "./shared-components/Label";
 import GroupRow from "./shared-components/GroupRow";
 
@@ -12,22 +22,64 @@ const styles = {
         backgroundColor: '#2D323B',
         border: '1px solid #000',
         borderRadius: '5px',
+    },
+    table: {
+        marginBottom: '2.5%',
+        // marginTop: '2.5%',
+        // backgroundColor: '#14161A',
+        backgroundColor: '#1F1F1F',
+        border: '1px solid lightGray',
+        borderRadius: '7px',
+        minHeight: '100%',
+        color: 'gray'
+    },
+    table_container: {
+        backgroundColor: 'unset'
     }
 };
+
+const HeaderTableCell = withStyles({
+    root: {
+        color: 'white',
+        fontSize: '16px',
+        borderRight: '1px solid lightGray',
+    }
+})(TableCell);
+
+const LastCell = withStyles({
+    root: {
+        color: 'white',
+        fontSize: '16px',
+    }
+})(TableCell);
 
 const GroupListings = ({ classes, groupListings }) => {
     return(
         <Container className={classes.root} maxWidth="md">
             <Label>Group Listings</Label>
-            { groupListings.map( group => { 
-                console.log(group)
-                return <GroupRow
+            <TableContainer className={classes.table_container}>
+                <Table className={classes.table}>
+                    <TableHead>
+                        <TableRow>
+                            <HeaderTableCell width={50}>ID</HeaderTableCell>
+                            <HeaderTableCell align="center">Host Name</HeaderTableCell>
+                            <HeaderTableCell align="center">Activity</HeaderTableCell>
+                            <LastCell align="center" width={150}>Experience</LastCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                    { groupListings.map( group => { 
+                        console.log(group)
+                        return <GroupRow
                             key={group.id}
                             id={group.id} 
                             hostName={group.hostName} 
                             activity={group.activity} 
                             experience={group.experience} />}) 
-            }
+                    }
+                    </TableBody>
+                </Table>
+            </TableContainer>
         </Container>
     )
 }
--- rs-lfg-front-end/src/components/shared-components/GroupRow.jsx
+++ rs-lfg-front-end/src/components/shared-components/GroupRow.jsx
@@ -1,84 +1,33 @@
 import React from 'react'
 import { withStyles } from '@material-ui/core/styles';
-
 import {
-    Container,
-    Grid
+    TableRow,
+    TableCell,
 } from "@material-ui/core";
 
-import { DataGrid } from '@material-ui/data-grid';
-
-import Label from "./Label";
-// import Moment from "moment";
-
-const styles = {
+const WhiteTableCell = withStyles({
     root: {
-        marginBottom: '2.5%',
-        marginTop: '2.5%',
-        backgroundColor: '#14161A',
-        border: '1px solid #000',
-        borderRadius: '5px',
-        height: '100px'
-    },
-    groupSection: {
-        textAlign: 'left'
-    },
-    dateSection: {
-        textAlign: 'right'
-    },
-    hostText: {
-        fontSize: '20px'
-    },
-    dateText: {
-        fontSize: '15px'
+        color: "#FFF",
+        borderRight: '1px solid #FFF',
     }
-};
+})(TableCell);
 
-const columns = [
-    { field: 'id', headerName: 'ID', width: 90 },
-    { field: 'hostName', headerName: 'Host Name', width: 150 },
-    { field: 'activity', headerName: 'Activity', width: 150 },
-    { field: 'experience', headerName: 'Experience', width: 90 }
-];
+const LastCell = withStyles({
+    root: {
+        color: 'white',
+        fontSize: '16px',
+    }
+})(TableCell);
 
-const GroupRow = ({ classes, id, hostName, activity, experience }) => {
-
-    const row = { id, hostName, activity, experience };
-
+const GroupRow = ({ id, hostName, activity, experience }) => {
     return (
-        <Container className={classes.root} maxWidth="md">
-            <DataGrid 
-                rows={row}
-                columns={columns}
-                pageSize={10}
-                disableSelectionOnClick
-            />
-{/*             
-            <Grid container direction="row" justify="center" alignItems="center">
-                <React.Fragment>
-                    <Grid item xs={3} className={classes.groupSection}>
-                        <Label>Id:</Label>
-                        <Label className={classes.hostText}>{id}</Label>
-                    </Grid>
-
-                    <Grid item xs={3} className={classes.groupSection}>
-                        <Label>Host Name:</Label>
-                        <Label className={classes.hostText}>{hostName}</Label>
-                    </Grid>
-
-                    <Grid item xs={3} className={classes.groupSection}>
-                        <Label>Activity:</Label>
-                        <Label className={classes.hostText}>{activity}</Label>
-                    </Grid>
-
-                    <Grid item xs={3} className={classes.groupSection}>
-                        <Label>Experience:</Label>
-                        <Label className={classes.hostText}>{experience}</Label>
-                    </Grid>
-                </React.Fragment>                    
-            </Grid> */}
-        </Container>
+        <TableRow>
+            <WhiteTableCell>{id}</WhiteTableCell>
+            <WhiteTableCell>{hostName}</WhiteTableCell>
+            <WhiteTableCell>{activity}</WhiteTableCell>
+            <LastCell>{experience}</LastCell>
+        </TableRow>
     )
 }
 
-export default withStyles(styles)(GroupRow)
+export default GroupRow
